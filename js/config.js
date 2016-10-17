db92e974143b1edd616c2322a1a06f61d3b0b2c9
antonshostak <antohash@gmail.com>
2016-10-17 23:54:19 +0300

    Initial commit

---
+++ js/config.js
@@ -0,0 +1,19 @@
+/**
+ * Created by User1 on 16/10/2016.
+ */
+var config = {
+    STB_Model:[
+        {STB:"ALL STBs",Value:"0"},
+        {STB:"All Altech",Value:"DTR-9401"},
+        {STB:"All ADB",Value:"NET-2721TW,NET-3720TWX"},
+        {STB:"ADB model NET-2721",Value:"NET-2721TW"},
+        {STB:"ADB model NET-3720",Value:"NET-3720TWX"}
+    ],
+    Tasks:[
+        {Name:"UPDATE_SW"},
+        {Name:"VOD_REFRESH"},
+        {Name:"DTT_SCAN"},
+        {Name:"REMOTE_LOG"}
+    ],
+    
+};
